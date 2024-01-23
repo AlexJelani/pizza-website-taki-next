91a65f6ccd58da5e4459d3de6b61f849922fc6a3
JAlexander <jelanialexander@gmail.com>
2024-01-23 18:04:28 +0900

    added new navBar

--- pizza-website-react-new/src/App.jsx
+++ pizza-website-react-new/src/App.jsx
@@ -2,13 +2,14 @@ import Home from "./pages/Home.jsx";
 import {Routes, Route,} from "react-router-dom";
 import Header from "./Components/Header.jsx";
 import Navbar from "./Components/Navbar.jsx";
+import ResponNavBar from "./Components/ResponNavBar.jsx";
 
 
 function App() {
 
   return (
       <div className="App">
-        <Navbar/>
+        <ResponNavBar/>
         <Routes>
             <Route path='/' element={<Home/>}/>
         </Routes>
