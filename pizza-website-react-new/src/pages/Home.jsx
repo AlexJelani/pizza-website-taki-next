48d830ffce1440c547030a57435eb7b2dd88b264
JAlexander <jelanialexander@gmail.com>
2024-01-31 18:01:05 +0900

    added MenuComponent.jsx

--- pizza-website-react-new/src/pages/Home.jsx
+++ pizza-website-react-new/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HomeSection from "../Components/HomeSection.jsx";
 import About from "../Components/About.jsx";
-import Menu from "../Components/Menu.jsx";
+import MenuCard from "../Components/MenuCard.jsx";
 import Contact from "../Components/Contact.jsx";
 import Featured from "../Components/Featured.jsx";
 import {MenuList} from "../Components/MenuList.jsx";
@@ -9,6 +9,7 @@ import menuData from "../data/menuData.jsx";
 import Contact2 from "../Components/Contact2.jsx";
 import Footer from "../Components/Footer.jsx";
 import ScrollToTop from "../Components/ScrollToTop.jsx";
+import MenuComponent from "../Components/MenuComponent.jsx";
 
 
 function Home() {
@@ -20,7 +21,8 @@ function Home() {
             {/* about section */}
             <About/>
             {/*about menu*/}
-            <MenuList menuData={menuData} id="menu"/>
+            {/*<MenuList menuData={menuData} id="menu"/>*/}
+            <MenuComponent/>
             {/*about contact*/}
             <Contact2/>
             {/*footer*/}
